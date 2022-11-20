6514c339af8fb9dade5203a2cdb8486f6deccce6
Ketevan Nanuashvili <100858778+KetiNanuashvili@users.noreply.github.com>
2022-11-20 23:30:17 +0400

    add header background

--- about.js
+++ about.js
@@ -8,6 +8,14 @@ toggleBar.addEventListener('click', function(){
     toggleBar.classList.toggle('toggleActive');
     navBar.classList.toggle('overlay');
 }) 
+const navbar = document.querySelector('.nav-fixed');
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
 
 //accordion
 let accordion = document.querySelectorAll('.container-about');
--- services.js
+++ services.js
@@ -7,6 +7,14 @@ toggleBar.addEventListener('click', function(){
     toggleBar.classList.toggle('toggleActive');
     navBar.classList.toggle('overlay');
 }) 
+const navbar = document.querySelector('.nav-fixed');
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
 //third section
 
 document.querySelectorAll('.three-divs').forEach(item => {
